2697d264550df8d70021e4d0e833cbe8236dea35
kiyung <kiyung@toss.im>
2022-12-04 17:33:38 +0900

    feat: script

---
+++ script/webpack.config.js
@@ -0,0 +1,45 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const webpack = require("webpack");
+const Dotenv = require("dotenv-webpack");
+
+const cwd = process.cwd();
+
+module.exports = {
+  mode: process.env === "production" ? "production" : "development",
+  entry: path.join(cwd, "src/index.tsx"),
+  output: {
+    publicPath: "/",
+    path: path.join(cwd, "dist"),
+    filename: "embed.js",
+  },
+  resolve: {
+    alias: { src: path.resolve(__dirname, "./src") },
+    extensions: [".js", ".cjs", ".mjs", ".jsx", ".ts", ".tsx"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(m?js|ts)x?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-typescript"],
+            plugins: ["@emotion/babel-plugin"],
+          },
+        },
+      },
+      { test: /\.(png|jpe?g)$/, use: { loader: "file-loader" } },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({ template: "public/index.html" }),
+    new Dotenv(),
+  ],
+  devServer: {
+    port: 3000,
+    historyApiFallback: true,
+    hot: true,
+  },
+};
